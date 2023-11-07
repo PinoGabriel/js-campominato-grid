e0e7eb70443bd35e27ab2872fbf964a24dbca435
PinoGabriel <pinogabriel01@gmail.com>
2023-11-07 17:56:00 +0100

    feat: added logo and footer

--- js/main.js
+++ js/main.js
@@ -6,61 +6,38 @@ let containerMain = document.getElementById("container_main")
 let level = document.getElementById("level")
 
 
-play.addEventListener("click", function(){
+
+play.addEventListener("click", function () {
+    containerMain.innerHTML = ""
 
     if (level.value == "easy") {
         console.log("selezionato easy");
-        for (let i = 1; i < 101; i++) {
-            console.log(i);
-            const div = document.createElement("div")
-            div.classList.add("square_easy");
-            containerMain.appendChild(div)
-        
-            div.addEventListener("click", function () {
-                if (div.innerHTML == "") {
-                    div.innerHTML = i;
-                } else {
-                    div.innerHTML = "";
-                }
-                div.classList.toggle("square_bg");
-            });
-        }
+        generaGriglia(100, "square_easy");
     } else if (level.value == "med") {
         console.log("selezionato med");
-        for (let i = 1; i < 82; i++) {
-            console.log(i);
-            const div = document.createElement("div")
-            div.classList.add("square_medium");
-            containerMain.appendChild(div)
-        
-            div.addEventListener("click", function () {
-                if (div.innerHTML == "") {
-                    div.innerHTML = i;
-                } else {
-                    div.innerHTML = "";
-                }
-                div.classList.toggle("square_bg");
-            });
-        }
+        generaGriglia(81, "square_medium")
     } else {
-        for (let i = 1; i < 50; i++) {
-            console.log(i);
-            const div = document.createElement("div")
-            div.classList.add("square_hard");
-            containerMain.appendChild(div)
-        
-            div.addEventListener("click", function () {
-                if (div.innerHTML == "") {
-                    div.innerHTML = i;
-                } else {
-                    div.innerHTML = "";
-                }
-                div.classList.toggle("square_bg");
-            });
-        }
+        generaGriglia(49, "square_hard")
     }
 
-    
-
-
 })
+
+
+
+function generaGriglia(numeroCelle, className) {
+    for (let i = 1; i <= numeroCelle; i++) {
+        console.log(i);
+        const div = document.createElement("div");
+        div.classList.add(className);
+        containerMain.appendChild(div);
+
+        div.addEventListener("click", function () {
+            if (div.innerHTML == "") {
+                div.innerHTML = i;
+            } else {
+                div.innerHTML = "";
+            }
+            div.classList.toggle("square_bg");
+        });
+    }
+}

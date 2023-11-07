7818922f45367d242c4482c9a63f8966bcfc9a42
PinoGabriel <pinogabriel01@gmail.com>
2023-11-07 17:00:43 +0100

    feat: added bonus

--- js/main.js
+++ js/main.js
@@ -1,14 +1,66 @@
 
+let play = document.getElementById("buttonPlay")
+
 let containerMain = document.getElementById("container_main")
 
-for (let i = 1; i < 101; i++) {
-    console.log(i);
-    const div = document.createElement("div")
-    div.classList.add("square");
-    containerMain.appendChild(div)
+let level = document.getElementById("level")
 
-    div.addEventListener("click", function(){
-        div.innerHTML = i
-        div.classList.toggle("square_bg")
-    })
-}
+
+play.addEventListener("click", function(){
+
+    if (level.value == "easy") {
+        console.log("selezionato easy");
+        for (let i = 1; i < 101; i++) {
+            console.log(i);
+            const div = document.createElement("div")
+            div.classList.add("square_easy");
+            containerMain.appendChild(div)
+        
+            div.addEventListener("click", function () {
+                if (div.innerHTML == "") {
+                    div.innerHTML = i;
+                } else {
+                    div.innerHTML = "";
+                }
+                div.classList.toggle("square_bg");
+            });
+        }
+    } else if (level.value == "med") {
+        console.log("selezionato med");
+        for (let i = 1; i < 82; i++) {
+            console.log(i);
+            const div = document.createElement("div")
+            div.classList.add("square_medium");
+            containerMain.appendChild(div)
+        
+            div.addEventListener("click", function () {
+                if (div.innerHTML == "") {
+                    div.innerHTML = i;
+                } else {
+                    div.innerHTML = "";
+                }
+                div.classList.toggle("square_bg");
+            });
+        }
+    } else {
+        for (let i = 1; i < 50; i++) {
+            console.log(i);
+            const div = document.createElement("div")
+            div.classList.add("square_hard");
+            containerMain.appendChild(div)
+        
+            div.addEventListener("click", function () {
+                if (div.innerHTML == "") {
+                    div.innerHTML = i;
+                } else {
+                    div.innerHTML = "";
+                }
+                div.classList.toggle("square_bg");
+            });
+        }
+    }
+
+    
+
+
+})
